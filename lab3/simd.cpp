b0406b07e508ba25812baed2133c71e101366523
Jakub Jakubowicz <95585985+kk3k02@users.noreply.github.com>
2023-05-12 09:23:08 +0000

    Update simd.cpp
    Pomiar czasu w ns z uzyciem biblioteki <chrono>

--- lab3/simd.cpp
+++ lab3/simd.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <xmmintrin.h>
 #include <time.h>
+#include <chrono>
 
 __m128 add(__m128 a, __m128 b);
 __m128 sub(__m128 a, __m128 b);
@@ -18,36 +19,39 @@ int main(){
 	
 	int count[3] = {2048, 4096, 8192}; // Ilosc liczb w wektorze
 	
-	unsigned long long result_add=0, result_sub=0, result_mul=0, result_div=0; // Zmienne do przechowywania czasow operacji
+	long long result_add=0, result_sub=0, result_mul=0, result_div=0; // Zmienne do przechowywania czasow operacji
 	
 	
 	for(int i=0; i<3; i++){
 		std::cout<<"Typ obliczen: SIMD / SISD\n";
 		std::cout<<"Liczba liczb:"<<count[i]<<"\n";
-		std::cout<<"Sredni czas [x]:\n";
+		std::cout<<"Sredni czas [ns]:\n";
 		
 		for(int j=0; j<10; j++){
 			__m128 operation;
-			unsigned long long start;
-		
+			
 			a = random_num(count[i], a);
 			b = random_num(count[i], b);
 			
-			start = timestamp();
+			auto start1 = std::chrono::high_resolution_clock::now();
 			operation = add(a, b);
-			result_add = result_add + (timestamp() - start);
+			auto end1 = std::chrono::high_resolution_clock::now();
+			result_add = result_add + std::chrono::duration_cast<std::chrono::nanoseconds>(end1-start1).count();
 			
-			start = timestamp();
+			auto start2 = std::chrono::high_resolution_clock::now();
 			operation = sub(a, b);
-			result_sub = result_sub + (timestamp() - start);
+			auto end2 = std::chrono::high_resolution_clock::now();
+			result_sub = result_sub + std::chrono::duration_cast<std::chrono::nanoseconds>(end2-start2).count();
 			
-			start = timestamp();
+			auto start3 = std::chrono::high_resolution_clock::now();
 			operation = mul(a, b);
-			result_mul = result_mul + (timestamp() - start);
+			auto end3 = std::chrono::high_resolution_clock::now();
+			result_mul = result_mul + std::chrono::duration_cast<std::chrono::nanoseconds>(end3-start3).count();
 			
-			start = timestamp();
+			auto start4 = std::chrono::high_resolution_clock::now();
 			operation = div(a, b);
-			result_div = result_div + (timestamp() - start);
+			auto end4 = std::chrono::high_resolution_clock::now();
+			result_div = result_div + std::chrono::duration_cast<std::chrono::nanoseconds>(end4-start4).count();
 			
 		}
 		
@@ -68,7 +72,120 @@ int main(){
 __m128 add(__m128 a, __m128 b){ // Dodawanie a+b
 	__m128 result;
 	
-	asm(
+	asm volatile(
+		"movups %1, %%xmm0\n"
+		"movups %2, %%xmm1\n"
+		"addps %%xmm1, %%xmm0\n"
+		"movups %%xmm0, %0"
+		:"=g" (result)
+		:"g" (a), "g" (b)
+		:"xmm0", "xmm1", "memory"
+		);
+	
+	return result;
+}
+
+__m128 sub(__m128 a, __m128 b){ // Odejmowanie a-b
+	__m128 result;
+	
+	asm volatile(
+		"movups %1, %%xmm0\n"
+		"movups %2, %%xmm1\n"
+		"subps %%xmm1, %%xmm0\n"
+		"movups %%xmm0, %0"
+		:"=g" (result)
+		:"g" (a), "g" (b)
+		:"xmm0", "xmm1", "memory"
+		);
+		
+	return result;
+}
+
+__m128 mul(__m128 a, __m128 b){ // Mnozenie a*b
+	__m128 result;
+
+	asm volatile(
+		"movups %1, %%xmm0\n"
+		"movups %2, %%xmm1\n"
+		"mulps %%xmm1, %%xmm0\n"
+		"movups %%xmm0, %0"
+		:"=g" (result)
+		:"g" (a), "g" (b)
+		:"xmm0", "xmm1", "memory"#include <iostream>
+#include <stdio.h>
+#include <xmmintrin.h>
+#include <time.h>
+#include <chrono>
+
+__m128 add(__m128 a, __m128 b);
+__m128 sub(__m128 a, __m128 b);
+__m128 mul(__m128 a, __m128 b);
+__m128 div(__m128 a, __m128 b);
+
+unsigned long long timestamp();
+
+__m128 random_num(int count, __m128 vec);
+
+int main(){
+
+	__m128 a, b; // Zmienne do wykonywania dzialan
+	
+	int count[3] = {2048, 4096, 8192}; // Ilosc liczb w wektorze
+	
+	long long result_add=0, result_sub=0, result_mul=0, result_div=0; // Zmienne do przechowywania czasow operacji
+	
+	
+	for(int i=0; i<3; i++){
+		std::cout<<"Typ obliczen: SIMD / SISD\n";
+		std::cout<<"Liczba liczb:"<<count[i]<<"\n";
+		std::cout<<"Sredni czas [ns]:\n";
+		
+		for(int j=0; j<10; j++){
+			__m128 operation;
+			
+			a = random_num(count[i], a);
+			b = random_num(count[i], b);
+			
+			auto start1 = std::chrono::high_resolution_clock::now();
+			operation = add(a, b);
+			auto end1 = std::chrono::high_resolution_clock::now();
+			result_add = result_add + std::chrono::duration_cast<std::chrono::nanoseconds>(end1-start1).count();
+			
+			auto start2 = std::chrono::high_resolution_clock::now();
+			operation = sub(a, b);
+			auto end2 = std::chrono::high_resolution_clock::now();
+			result_sub = result_sub + std::chrono::duration_cast<std::chrono::nanoseconds>(end2-start2).count();
+			
+			auto start3 = std::chrono::high_resolution_clock::now();
+			operation = mul(a, b);
+			auto end3 = std::chrono::high_resolution_clock::now();
+			result_mul = result_mul + std::chrono::duration_cast<std::chrono::nanoseconds>(end3-start3).count();
+			
+			auto start4 = std::chrono::high_resolution_clock::now();
+			operation = div(a, b);
+			auto end4 = std::chrono::high_resolution_clock::now();
+			result_div = result_div + std::chrono::duration_cast<std::chrono::nanoseconds>(end4-start4).count();
+			
+		}
+		
+		std::cout<< "+ " << result_add/10<<"\n";
+		std::cout<< "- " << result_sub/10<<"\n";
+		std::cout<< "* " << result_mul/10<<"\n";
+		std::cout<< "/ " << result_div/10<<"\n\n";
+	}
+	
+	__m128 _add = add(a, b);
+	__m128 _sub = sub(a, b);
+	__m128 _mul = mul(a, b);
+	__m128 _div = div(a, b);
+	
+	
+}
+
+__m128 add(__m128 a, __m128 b){ // Dodawanie a+b
+	__m128 result;
+	
+	asm volatile(
 		"movups %1, %%xmm0\n"
 		"movups %2, %%xmm1\n"
 		"addps %%xmm1, %%xmm0\n"
@@ -129,22 +246,44 @@ __m128 div(__m128 a, __m128 b){ // Dzielenie a/b
 	return result;
 }
 
-unsigned long long timestamp(){ // Pomiar ilosci cyklow procesora
-	unsigned long long result;
-
-	asm volatile(
-		"xor %%eax, %%eax\n"
-		"cpuid\n"
-		"rdtsc"
-		:"=A" (result)
-		:
-		:"ebx", "ecx"
+__m128 random_num(int count, __m128 vec){ // Wypelnianie wektora losowymi liczbami
+	srand(time(NULL));
+	
+	int* table = new int[count];
+	
+	for(int i=0; i<count; i++){
+		table[i] = (std::rand() % RAND_MAX) + 1;
+	}
+	
+	__m128i int_vec = _mm_load_si128((__m128i*)table);
+	__m128 float_vec = _mm_castsi128_ps(int_vec);
+	float_vec = _mm_add_ps(float_vec, vec);
+	int_vec = _mm_castps_si128(float_vec);
+	
+	return _mm_castsi128_ps(int_vec);
+}
 	);
 	
 	return result;
 }
 
-__m128 random_num(int count, __m128 vec){
+__m128 div(__m128 a, __m128 b){ // Dzielenie a/b
+	__m128 result;
+
+	asm volatile(
+		"movups %1, %%xmm0\n"
+		"movups %2, %%xmm1\n"
+		"divps %%xmm1, %%xmm0\n"
+		"movups %%xmm0, %0"
+		:"=g" (result)
+		:"g" (a), "g" (b)
+		:"xmm0", "xmm1", "memory"
+	);
+	
+	return result;
+}
+
+__m128 random_num(int count, __m128 vec){ // Wypelnianie wektora losowymi liczbami
 	srand(time(NULL));
 	
 	int* table = new int[count];
